d9b7c16c0b522d0f18f51dc49930eccf466317c3
yahya <yalbarrawoui256@gmail.com>
2018-02-07 09:33:45 +0200

    contanct

--- src/client/components/contact/Contact.js
+++ src/client/components/contact/Contact.js
@@ -11,21 +11,27 @@ class Contact extends Component {
     <li>
       <h4 className="footer-list-header">Oppening Hours</h4></li>
     <li><a className="footer-list-anchor">Monday - Friday</a></li>
-    <li><a className="footer-list-anchor">9:00 am - 6:00 pm</a></li>
+    <li><a className="footer-list-anchor2">9:00 am - 6:00 pm</a></li>
+    <hr/>
     <li><a className="footer-list-anchor">Saturday</a></li>
-    <li><a className="footer-list-anchor">9:00 am - 4:00 pm</a></li>
+    <li><a className="footer-list-anchor2">9:00 am - 4:00 pm</a></li>
+    <hr/>
     <li><a className="footer-list-anchor">Sunday</a></li>
-    <li><a className="footer-list-anchor">9:00 am - 2:00 pm</a></li>
+    <li><a className="footer-list-anchor2">9:00 am - 2:00 pm</a></li>
+    <hr/>
   </ul>
-  <ul className="footer-list-top">
+  <ul className="footer-list-top vl">
     <li>
       <h4 className="footer-list-header">Contact Us</h4></li>
     <li><a className="footer-list-anchor">Gaza</a></li>
-    <li><a className="footer-list-anchor">+972591234567</a></li>
+    <li><a className="footer-list-anchor2">+972591234567</a></li>
+    <hr/>
     <li><a className="footer-list-anchor">Hebron</a></li>
-    <li><a className="footer-list-anchor">+972591234567</a></li>
+    <li><a className="footer-list-anchor2">+972591234567</a></li>
+    <hr/>
     <li><a className="footer-list-anchor">Juresalem</a></li>
-    <li><a className="footer-list-anchor">+972591234567</a></li>
+    <li><a className="footer-list-anchor2">+972591234567</a></li>
+    <hr/>
   </ul>
 </section>
      </div>
